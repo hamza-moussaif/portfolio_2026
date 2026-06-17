3b306b4f2119b56f3f30e9ea26f730fe68e77e7c
hamza moussaif <hamza.moussaif@emsi-edu.ma>
2026-06-17 13:13:36 +0100

    retry

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    ssr: false,
+    server: {
+      preset: "netlify",
+    },
   },
 });
