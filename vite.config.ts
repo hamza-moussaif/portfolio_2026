14f10b9bbd24d330c9d58f54ec1db6b4c23a11ab
hamza moussaif <hamza.moussaif@emsi-edu.ma>
2026-06-17 12:29:41 +0100

    fix4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import { TanStackStartVite } from "@tanstack/react-start/plugin";
+import { TanStackStartVite } from "@tanstack/start-vite-plugin";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
       },
     }),
     react(),
+    tailwindcss(),
   ],
 });
