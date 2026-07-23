f0ebdae27b6dd184677ead11009256144e3d5045
hamza moussaif <hamza.moussaif@emsi-edu.ma>
2026-07-23 11:26:32 +0100

    fix 10

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   nitro: {
     preset: "netlify",
+    output: {
+      dir: ".netlify/functions-internal",
+      serverDir: ".netlify/functions-internal/server",
+      publicDir: "dist",
+    },
   },
 });
